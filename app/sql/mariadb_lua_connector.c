4c05d21dcb7b5698df0f370ad618c41fd3338ed9
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-18 23:23:11 +0200

    Does it works?

--- app/sql/mariadb_lua_connector.c
+++
@@ -1,8 +0,0 @@
-#include <mariadb/mysql.h>
-
-#include <lua5.3/lua.h>
-#include <lua5.3/lualib.h>
-#include <lua5.3/lauxlib.h>
-
-	return 0;
-}
--- mariadb_connector.c
+++ mariadb_connector.c
@@ -13,6 +13,7 @@ int mariadb_execute_select(lua_State *L){
 	}
 	if(!lua_isstring(L,-1))return 0;
 	if(mysql_query(conn,lua_tostring(L,-1))){
+		lua_pop(L,1);
 		return 0;
 	}
 	lua_pop(L,1);
