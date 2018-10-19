07b8ffdc4993024ac719b0c0a88b2827ed3a3f01
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-19 05:00:11 +0200

    Does it work?

--- app/app.lua
+++ app/app.lua
@@ -41,7 +41,7 @@ function process_request(http_request)
 				local s,n=string.gsub(GET_value,"/api%.json%?(.+)","%1")
 				if n>0 then
 					local t={}
-					string.gsub(s,".*(w-)=(w-).*",function(a,b) t[a]=b end)
+					string.gsub(s,".*(w-)=(w-).*",function(a) t[a[1]]=a[2] end)
 					for k,v in pairs(t) do
 						print(k.."|"..v)
 					end
