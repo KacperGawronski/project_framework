fdbfc8fca829a93087d4c7b11fcc1f4a812c3239
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-19 21:20:56 +0200

    Fixing

--- app/javascript/example.js
+++ app/javascript/example.js
@@ -1,7 +1,6 @@
 var client = new XMLHttpRequest();
-client.open('GET', '/api.json?limits=30');
+client.open('GET', '/api.json?limit=30');
 client.onreadystatechange = function() {
-  alert(client.responseText);
   document.getElementById("example").innerHTML = client.responseText;
 }
 client.send();
