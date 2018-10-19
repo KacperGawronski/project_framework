edc57867484e2bd12f33b24f415f6f75f01cc662
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-19 04:04:49 +0200

    Does it work?

--- app/app.lua
+++ app/app.lua
@@ -40,6 +40,7 @@ function process_request(http_request)
 				coroutine.yield("</body></html>")
 			else
 				local s,n=string.gsub(GET_value,"/api.json?(.+)")
+				print(s,n)
 				if n>0 then
 					local tmp=mariadb_execute_select("SELECT * FROM employees LIMIT 10")
 					print(tmp)
