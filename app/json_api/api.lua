c153d7cec57a0d0466d8498756e45d362918032c
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-19 19:17:54 +0200

    Does it work?

--- app/json_api/api.lua
+++ app/json_api/api.lua
@@ -3,8 +3,8 @@ function SELECT(s)
 	local t={}
 	local limit=30
 	local data_table="employees"
-	print(s)
 	s=string.gsub(s,"'","''"):gsub("%3E",">"):gsub("%3C","<")
+	print(s)
 	for k,sign,v in s:gmatch("([%w_,]+)([<=>]+)([%w_,]+)") do
 		if k=="limit" then
 			local tmp
