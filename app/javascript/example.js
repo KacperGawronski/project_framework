f9cd473d14f62afdf0150a6e9bac7c2d9dad1914
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-20 21:01:45 +0200

    js

--- app/javascript/example.js
+++ app/javascript/example.js
@@ -1,20 +1,20 @@
 function get_value(){
 	var client = new XMLHttpRequest();
-	var contentTEXT = "<table>"
-	function parse_obj(Obj){
-		contentTEXT +="<tr>"
-		for(k in Obj){
-			contentTEXT +="<td>"+Obj[k]+"</td>"
-		}
-		contentTEXT +="</tr>"
-	}	
 	client.open('GET', '/api.json?limit=30');
-	client.onreadystatechange = function() {
-		document.getElementById("example").innerHTML = JSON.parse(client.responseText)[0]["emp_no"]
+	client.onreadystatechange = function() {		
+		var contentTEXT = "<table>"
+		function parse_obj(Obj){
+			contentTEXT +="<tr>"
+			for(k in Obj){
+				contentTEXT +="<td>"+Obj[k]+"</td>"
+			}
+			contentTEXT +="</tr>"
+		}	
+		JSON.parse(client.responseText).forEach(parse_obj)
+		contentTEXT +="</table>";
+		document.getElementById("example").innerHTML = contentTEXT
 	}
 	client.send();
-	contentTEXT +="</table>";
-	return contentTEXT;	
 }
 
 get_value()
