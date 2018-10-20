a75363775dce33f8f153ebc76479aba3d1d3099d
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-21 00:05:35 +0200

    js

--- app/javascript/example.js
+++ app/javascript/example.js
@@ -20,7 +20,7 @@ function get_val_from_request(){
 		}	
 		var responseArray = JSON.parse(client.responseText);
 		for (k in responseArray[0]){
-			contentTEXT+="<td><form action=\"javascript:get_val_from_request();\" onsubmit=\"get_val_from_request()\" id=\""+k+"\">"+k+"</br><input class=\"QUERY\" value=\"\"></form></td>"
+			contentTEXT+="<td><form action=\"javascript:get_val_from_request();\" id=\""+k+"\">"+k+"</br><input class=\"QUERY\" value=\"\"></form></td>"
 		}
 		contentTEXT += "</tr>"
 		responseArray.forEach(parse_obj);
@@ -28,7 +28,6 @@ function get_val_from_request(){
 		document.getElementById("example").innerHTML = contentTEXT
 	}
 	client.send();
-	
 }
 
 function get_value(){
