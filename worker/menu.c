045c7eb5dc47962c8f49936b6ac0de643c88057e
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-20 15:52:24 +0200

    Well, not works

--- worker/menu.c
+++ worker/menu.c
@@ -55,6 +55,13 @@ static void make_tree(lua_State *L, char *path){
 }
 
 int generate_menu(lua_State *L){
+	char cur_item[256];
+	char cur_name[128];
+	DIR *dir;
+	struct dirent *entry;
+	int l;
+	
+	
 	char *str;
 	str=lua_tostring(L,-1);
 	lua_pop(L,1);
@@ -64,7 +71,29 @@ int generate_menu(lua_State *L){
 	lua_pushnumber(L,strings_amount);
 	lua_pushstring(L,"<menu class=\"main_menu\"><ul class=\"menu_list\">");
 	lua_settable(L,-3);
-	make_tree(L,str);
+	/*singlefunctioning*/
+	
+	if ((dir = opendir(path)) != NULL){
+		while ((entry = readdir(dir)) != NULL){
+			switch(entry->d_type){
+				case DT_REG:
+					if(!strcmp(".lua",&(entry->d_name[strlen(entry->d_name)-4]))){
+						strncpy(cur_name,entry->d_name,strlen(entry->d_name)-4);
+						cur_name[strlen(entry->d_name)-4]='\0';
+						snprintf(cur_item,255,"<li class=\"menu_button\"><a class=\"menu_link\" href=\"page?%s\">%s</a></li>",cur_name,cur_name);
+						++strings_amount;
+						lua_pushnumber(L,strings_amount);
+						lua_pushstring(L,cur_item);
+						lua_settable(L,-3);
+					}
+					break;
+			}
+		}	
+	closedir(dir);
+	}
+	
+	/*end*/
+	
 	++strings_amount;
 	lua_pushnumber(L,strings_amount);
 	lua_pushstring(L,"</menu></ul>");
