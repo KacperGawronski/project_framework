e8cf8fdbbbe2b17e0f56ec26e4ad0d866eb92c89
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-20 20:33:07 +0200

    css

--- app/javascript/example.js
+++ app/javascript/example.js
@@ -1,23 +1,18 @@
 
-function gen_value(){
-	var client = new XMLHttpRequest();
-	var contentTEXT="";
-	function parse_obj(Obj){
-		contentTEXT +="<tr>"
-		for(k in Obj){
-			contentTEXT +="<td>"+Obj[k]+"</td>"
-		}
-		contentTEXT +="</tr>"
+var client = new XMLHttpRequest();
+var contentTEXT = "<table>"
+function parse_obj(Obj){
+	contentTEXT +="<tr>"
+	for(k in Obj){
+		contentTEXT +="<td>"+Obj[k]+"</td>"
 	}
-
-	client.open('GET', '/api.json?limit=30');
-	client.onreadystatechange = function() {
-	  contentTEXT += "<table>"
-	  (JSON.parse(client.responseText)).forEach(parse_obj)
-	  contentTEXT +="</table>";
-	}
-	client.send();
-	document.getElementById("example").innerHTML = contentTEXT;
+	contentTEXT +="</tr>"
 }
 
-gen_value();
+client.open('GET', '/api.json?limit=30');
+client.onreadystatechange = function() {
+  (JSON.parse(client.responseText)).forEach(parse_obj)
+  contentTEXT +="</table>";
+}
+client.send();
+document.getElementById("example").innerHTML = contentTEXT;
