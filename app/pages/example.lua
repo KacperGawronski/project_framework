55b0b4cca6daa76015313c0d6b50774da70a5c67
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-21 16:54:31 +0200

    js

--- app/pages/example.lua
+++ app/pages/example.lua
@@ -27,6 +27,7 @@ local function f()
 	page_structure.script="<script src=\"example.js\"></script>"
 	page_structure.body=function ()
 		local function f()
+			coroutine.yield("<form action=\"get_value()\"><input defaultvalue=\"30\" name=\"limit\"></input></form>")
 			coroutine.yield("<div id=\"main\"><div id=\"example\"></div></div>")
 		end
 		return coroutine.wrap(function () f() end)
