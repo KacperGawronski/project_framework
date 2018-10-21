d33ae687d4d75a45ec7aea884e5c4bab69ade3e5
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-21 19:50:24 +0200

    Fix

--- app/pages/index.lua
+++ app/pages/index.lua
@@ -27,7 +27,7 @@ local function f()
 	page_structure.script=""
 	page_structure.body=function ()
 		local function f()
-			coroutine.yield("<div id=\"main\"><h2>Welcome to open example Framework based on Lua_WebServer and Example Lua App</h2></br>")
+			coroutine.yield("<div id=\"main\"><h2>Welcome to open example Project Framework based on Lua_WebServer and Example Lua App</h2></br>")
 			coroutine.yield("You can find them on github:</br>\
 <a href=\"https://github.com/KacperGawronski/Lua_Webserver\">Lua_Webserver</a></br>\
 <a href=\"https://github.com/KacperGawronski/example_Lua_app\">example_Lua_app</a></br>")
