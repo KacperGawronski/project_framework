9f3f1baf39a2ad16b6e3fb089c25d62b465d762a
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-21 16:18:47 +0200

    Cleaning

--- app/index.lua
+++
@@ -1,7 +0,0 @@
-function generate_index()
-	return "<h2>Welcome to open example of Lua_WebServer and Example Lua App</h2></br>\
-You can find them on github:</br>\
-<a href=\"https://github.com/KacperGawronski/Lua_Webserver\">Lua_Webserver</a></br>\
-<a href=\"https://github.com/KacperGawronski/example_Lua_app\">example_Lua_app</a></br>\
-Both are licensed under GNU General Public License v3"
-end
--- app/menu.lua
+++
@@ -1,5 +0,0 @@
-function generate_menu()
-	return "<ul>\
-<li><a href=\"/\">index</a></li>\
-</ul>"
-end
