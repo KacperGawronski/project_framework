5e142574affd4c1f6abb252c7ff0ddc3b2cd24c2
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-23 17:49:39 +0200

    Working on cookies, trying to find and fix a bug. (Found one)

--- app/get_cookies.lua
+++ app/get_cookies.lua
@@ -1,7 +1,7 @@
 return function(request)
 	local t={}
 	for content in string.gmatch(request,"Cookie: (.-)\r\n") do 
-		for k,v in content do
+		for k,v in string.gmatch(content,"(%w+)=(%w+)") do
 			t[k]=v end
 		end
 	return t
