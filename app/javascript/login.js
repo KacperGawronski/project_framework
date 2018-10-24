f7014d288fa07965b484ebb829964dab9dd25721
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2018-10-24 02:25:11 +0200

    Almost testing cookies, testing js I

--- app/javascript/login.js
+++ app/javascript/login.js
@@ -1,3 +1,3 @@
-document.onload = function(){
-	document.getElementById("login").innerHTML = "<form id=\"auth_form\">Login:</br><input id=\"login\"></br>Password</br><input type=\"password\"></form>"
+window.onload = function(){
+	document.getElementById("login").innerHTML = '<form id="auth_form">Login:</br><input id="login"></br>Password</br><input type="password"></form>'
 }
